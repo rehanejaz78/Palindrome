51eb02a8a266bd4a0c338314b5bf0e2c4e78446a
rehanejaz78 <82099442+rehanejaz78@users.noreply.github.com>
2023-01-05 14:18:59 +0500

    Palindrome Java

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+        System.out.println(obj.isPalindrome("A man, a plan, a canal: Panama"));
+
+    }
+}
---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,47 @@
+package org.example;
+
+public class Solution {
+
+
+        public boolean isPalindrome(String s) {
+           String myString = "";
+            for (char c:s.toCharArray()) {
+                if (Character.isLetterOrDigit(c) ){
+                    myString +=c;
+                }
+            }
+
+            myString = myString.toLowerCase();
+//            int right = myString.length()-1;
+//            int left = 0;
+//
+//            while (left <= right){
+//
+//                if (myString.charAt(left) != myString.charAt(right) ){
+//                    return false;
+//                }
+//                else
+//                {
+//                    left++;
+//                    right--;
+//                }
+//            }
+//            return true;
+
+
+//            String reverse = "";
+//
+//            for (int i = myString.length()-1; i >= 0; i--) {
+//
+//                reverse += myString.charAt(i);
+//            }
+//            if (reverse.equals(myString))
+//                return true;
+//            else
+//                return false;
+
+            StringBuilder sb = new StringBuilder(myString);
+            return myString.equalsIgnoreCase((sb.reverse()).toString());
+    }
+
+}
